1501ad628dde2b6b31c9c1c98a88ac01faa3105f
lchjczw <lchjczw@163.com>
2019-10-16 02:24:54 +0800

    env

--- env.go
+++ env.go
@@ -2,21 +2,11 @@ package ui
 
 import (
 	"errors"
-	"fmt"
-	"os"
 	"os/exec"
 )
 
 func SetRunMod(is_debug bool) {
 	debug = is_debug
-}
-
-//如果是Windows系统，则添加一下路径
-func Init() {
-	//if runtime.GOOS == "windows" {
-	AddToPath(fmt.Sprintf(`C:\Users\%s\AppData\Local\Google\Chrome\Application\`, os.Getenv("USERNAME")))
-	//}
-
 }
 
 func CheckEnv() error {
@@ -26,6 +16,7 @@ func CheckEnv() error {
 	return nil
 }
 
+//reapleace the chromedp's function
 func FindChromePath() string {
 	for _, path := range [...]string{
 		// Unix-like
@@ -43,7 +34,7 @@ func FindChromePath() string {
 		"chrome",
 		"chrome.exe", // in case PATHEXT is misconfigured
 		`C:\Program Files (x86)\Google\Chrome\Application\chrome.exe`,
-		fmt.Sprintf(`C:\Users\%s\AppData\Local\Google\Chrome\Application\`, os.Getenv("USERNAME")),
+		//fmt.Sprintf(`C:\Users\%s\AppData\Local\Google\Chrome\Application\`, os.Getenv("USERNAME")),
 		// Mac
 		`/Applications/Google Chrome.app/Contents/MacOS/Google Chrome`,
 	} {
